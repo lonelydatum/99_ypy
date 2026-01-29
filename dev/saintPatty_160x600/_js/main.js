b24e1bcae6c9e34b97cce9ae8aceb0b75e880433
Gar <gar@lonelydatum.com>
2026-01-29 17:17:57 -0500

    done

--- dev/saintPatty_160x600/_js/main.js
+++ dev/saintPatty_160x600/_js/main.js
@@ -1,3 +1,12 @@
-import { init } from "../../_common/js/common.js";
+import { frameEnd, frame1, read } from "../../_common/js/common.js";
 
+function init() {
+  const tl = frame1();
+  tl.from(".device", { duration: 0.5, opacity: 0 });
+
+  tl.from(".t1", { duration: 0.5, opacity: 0 }, "+=.3");
+  tl.from(".t2", { duration: 0.3, opacity: 0 }, `+=2.2`);
+
+  frameEnd(tl);
+}
 init();
